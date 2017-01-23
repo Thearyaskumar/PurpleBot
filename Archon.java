64a35c8d7f14ba078430ea66f44ad5e29d22b177
Arya Kumar <2019akumar2@tjhsst.edu>
2017-01-23 11:10:42 -0500

    Added bugMove Testing

--- Archon.java
+++ Archon.java
@@ -3,6 +3,6 @@ import battlecode.common.*;
 
 public strictfp class Archon extends Globals {
     public void loop(){
-
+    	Movement.bugMove(new MapLocation(0,0));
     }
 }
--- Movement.java
+++ Movement.java
@@ -2,5 +2,7 @@ package PurpleBot;
 import battlecode.common.*;
 //Call the appropriate movement method from here, eg. Movement.bugMove(MapLocation m)
 public strictfp class Movement {
-    
+    public void bugmove(MapLocation m){
+    	
+    }
 }
